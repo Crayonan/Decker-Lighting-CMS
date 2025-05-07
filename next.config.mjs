9a78a8df2fdb9170607b8bd327dda31523244225
Crayonan <78920025+Crayonan@users.noreply.github.com>
2025-05-07 23:48:03 +0200

    Redirect to /admin and skip frontend

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,15 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your Next.js config here
+  async redirects() {
+    return [
+      {
+        source: '/', // Redirect from the root path
+        destination: '/admin', // Redirect to the Payload admin path
+        permanent: true, // Use false for non-SEO related redirects like this
+      },
+    ]
+  },
 }
 
 export default withPayload(nextConfig, { devBundleServerPackages: false })
